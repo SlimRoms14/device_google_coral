6adbceea2816f058fa7e1868a86ac1e3bd0e6d24
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    floral: Declare EUICC permission
    This is a squash of the following:
    
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Sat Dec 1 13:53:00 2018 -0800
    
        floral: Declare EUICC permission
    
        Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Oct 18 00:06:22 2019 -0500
    
        floral: Move eUICC permissions xml to /product
    
        All eUICC items were moved to /product in Q.
    
        Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab
    
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- device-slim.mk
+++ device-slim.mk
@@ -7,3 +7,7 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-slim
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
